4c25cb056292aa4ce5d3af14be18f62b31e65488
Jonathan Neufeld <jneufeld@alumni.ubc.ca>
2021-02-16 18:33:36 -0800

    Add post action decoding and display

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,16 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
+import Json.Decode as Decode
+    exposing
+        ( Decoder
+        , decodeString
+        , map2
+        , field
+        , index
+        , int
+        , string
+        )
 
 
 -- MAIN
@@ -20,14 +30,52 @@ main =
 type alias Model =
     { prevActions : List String
     , nextActions : List String
+    , hands : String
     }
 
 
 init : Model
 init =
-    Model [] [ "Fold", "Raise", "Fold", "Call" ]
+    Model [] [ "Fold", "Raise", "Fold", "Call" ] """{
+        "Post": [
+          "c2tiA/SMUK+T0PsP2rCOGA",
+          {
+            "fraction": 0,
+            "integer": 5
+          }
+        ]
+      }"""
 
 
+type alias Amount =
+    { integer: Int
+    , fraction: Int
+    }
+
+amountDecoder : Decoder Amount
+amountDecoder =
+    map2 Amount
+        (field "integer" int)
+        (field "fraction" int)
+
+amountText : Amount -> String
+amountText amount =
+    "$" ++ (String.fromInt amount.integer) ++ "." ++ (String.fromInt amount.fraction)
+
+type alias Post =
+    { playerId: String
+    , amount: Amount
+    }
+
+postDecoder : Decoder Post
+postDecoder =
+    map2 Post
+        (field "Post" (index 0 string))
+        (field "Post" (index 1 amountDecoder))
+
+postText : Post -> String
+postText post =
+    post.playerId ++ " posts " ++ (amountText post.amount)
 
 -- UPDATE
 
@@ -83,6 +131,7 @@ view model =
         [ button [ onClick PreviousAction ] [ text "Previous Action" ]
         , button [ onClick NextAction ] [ text "Next Action" ]
         , div [] [ text ("Action: " ++ actionText model.nextActions) ]
+        , div [] [text ("Hand: " ++ handText model.hands)]
         ]
 
 
@@ -94,3 +143,10 @@ actionText actions =
 
         action :: _ ->
             action
+
+
+handText : String -> String
+handText hands =
+    case decodeString postDecoder hands of
+       Ok post -> postText post
+       Err error -> "Error decoding hands"
